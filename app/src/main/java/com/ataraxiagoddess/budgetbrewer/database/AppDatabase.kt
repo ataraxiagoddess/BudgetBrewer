e1ffddd6589aa625532e7e500764b8c6b47c62ea
AtaraxiaGoddess <159365586+ataraxiagoddess@users.noreply.github.com>
2026-06-21 21:52:02 -0400

    cleanup

--- app/src/main/java/com/ataraxiagoddess/budgetbrewer/database/AppDatabase.kt
+++ app/src/main/java/com/ataraxiagoddess/budgetbrewer/database/AppDatabase.kt
@@ -5,7 +5,6 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.work.impl.Migration_15_16
 import com.ataraxiagoddess.budgetbrewer.data.*
 
 @Database(
